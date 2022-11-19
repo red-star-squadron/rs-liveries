30002656e37be3ab8856676dd95e731762613f84
Kaurin <2141359+Kaurin@users.noreply.github.com>
2022-11-19 21:56:22 +0000

    - Removal of download concurrency and bugfixes
    -- rs-skin-downloader.py
    * Removed concurrent execution for now
    * Minor bug fixes for the downloader (It would download top-level files
        as directories)
    * Livery secrets are stored on disk instead of using directly from
        env vars
    * Livery secrets format changed from Json to Yaml
    * Use os.path.join() wherever possible instead of string manipulation
    * Moved some comments to doc/devnotes.md
    
    -- doc/devnotes.md
    * New file with details on dev stuff
    
    -- Pipenv files refresh
    * Removal of ray
    * Added pyyaml
    
    -- Vscode related files
    * .env
    * .vscode/launch.json
    
    -- Github workflows
    * Livery secrets are stored on disk instead of using directly from
        env vars

--- rs-skin-downloader.py
+++ rs-skin-downloader.py
@@ -13,9 +13,8 @@ import logging
 from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 from googleapiclient import discovery
 import google.auth
-import ray
 import time
-import json
+import yaml
 
 # To list folders
 def listfolders(service, filid, des):
@@ -24,19 +23,19 @@ def listfolders(service, filid, des):
         fields="nextPageToken, files(id, name, mimeType)").execute()
     folder = results.get('files', [])
     for item in folder:
+        fullpath = os.path.join(des, item['name'])
         if str(item['mimeType']) == str('application/vnd.google-apps.folder'):
-            if not os.path.isdir(des+"/"+item['name']):
-                os.mkdir(path=des+"/"+item['name'])
+            if not os.path.isdir(fullpath):
+                os.mkdir(path=fullpath)
             print(item['name'])
-            listfolders(service, item['id'], des+"/"+item['name'])  # LOOP un-till the files are found
+            listfolders(service, item['id'], fullpath)  # LOOP un-till the files are found
         else:
-            downloadfiles(service, item['id'], item['name'], des)
+            downloadfiles(service, item['id'], fullpath)
             print(item['name'])
     return folder
 
-
 # To Download Files
-def downloadfiles(service, dowid, name,dfilespath):
+def downloadfiles(service, dowid, dfilespath):
     request = service.files().get_media(fileId=dowid)
     fh = io.BytesIO()
     downloader = MediaIoBaseDownload(fh, request)
@@ -44,12 +43,11 @@ def downloadfiles(service, dowid, name,dfilespath):
     while done is False:
         status, done = downloader.next_chunk()
         # print("Download %d%%." % int(status.progress() * 100))
-    with io.open(dfilespath + "/" + name, 'wb') as f:
+    with io.open(dfilespath, 'wb') as f:
         fh.seek(0)
         f.write(fh.read())
 
-@ray.remote
-def downloadfolder(rootfolder, folderid, service):
+def download_root_folder(rootfolder, folderid, service):
     folderid="'"+folderid+"'" # surrounding ' Needed for the "q" parameter to google drive's "list" API call
     results = service.files().list(
         pageSize=1000, q=folderid+" in parents", fields="nextPageToken, files(id, name, mimeType)").execute()
@@ -59,21 +57,22 @@ def downloadfolder(rootfolder, folderid, service):
     else:
         # print('Files:')
         for item in items:
-            if not os.path.isdir(rootfolder):
+            if rootfolder and not os.path.isdir(rootfolder): # If rootfolder is defined, and if the directory does not exist
                 os.mkdir(rootfolder)
-            bfolderpath = os.getcwd()+"/%s/" % rootfolder
-            if not os.path.isdir(bfolderpath + item['name']):
-                os.mkdir(bfolderpath + item['name'])
+            fullpath = os.path.join(os.getcwd(), rootfolder, item['name'])
             if item['mimeType'] == 'application/vnd.google-apps.folder':
-                folderpath = bfolderpath + item['name']
-                listfolders(service, item['id'], folderpath)
+                if not os.path.isdir(fullpath):
+                    os.mkdir(fullpath)
+                listfolders(service, item['id'], fullpath)
             else:
-                filepath = bfolderpath + item['name']
-                downloadfiles(service, item['id'], item['name'], filepath)
+                downloadfiles(service, item['id'], fullpath)
+                print(fullpath) # TODO Make a normal print
 
 def main():
     creds, _ = google.auth.default()
     service = build('drive', 'v3', credentials=creds)
+    with open('gdrive_secret.yml', 'r') as file:
+        Folders = yaml.safe_load(file)
 
     if os.path.isdir("Staging"):
         shutil.rmtree("Staging")
@@ -82,46 +81,16 @@ def main():
     os.mkdir("Staging")
     os.chdir("Staging")
 
-    ##
-    ##       !!!!! If updating these values with new skins, please ensure to update the .nsi script !!!!!!!
-    ##
-    ## Format of the ingested env vars looks like this:
-    # {
-    #     "mig-29s"       : "GOOGLE-DRIVE-SHARED-FOLDER-ID",
-    #     "mig-29a"       : "GOOGLE-DRIVE-SHARED-FOLDER-ID",
-    #     "mig-29g"       : "GOOGLE-DRIVE-SHARED-FOLDER-ID",
-    #     "jf-17"         : "GOOGLE-DRIVE-SHARED-FOLDER-ID",
-    #     "j-11a"         : "GOOGLE-DRIVE-SHARED-FOLDER-ID",
-    #     "su-27"         : "GOOGLE-DRIVE-SHARED-FOLDER-ID",
-    #     "f-16c_50"      : "GOOGLE-DRIVE-SHARED-FOLDER-ID",
-    #     "fa-18c_hornet" : "GOOGLE-DRIVE-SHARED-FOLDER-ID",
-    #     "f-15c"         : "GOOGLE-DRIVE-SHARED-FOLDER-ID"
-    # }
-    ##
-    Folders_RS = json.loads(os.environ['GOOGLE_DRIVE_RS_SKINS'])
-    Folders_RSC = json.loads(os.environ['GOOGLE_DRIVE_RSC_SKINS'])
-    # Future skin categories if added here must be added a few lines below (dl_list loop)
-
-    ray.init()
-    ray_obj_refs = []
-    for dl_list in [Folders_RS, Folders_RSC]:
-        for rootfolder, Folder_id in dl_list.items():
-           ray_obj_refs.append(downloadfolder.remote(rootfolder, Folder_id, service))
-    print("Started waiting for parallel downloads...")
-    ray.wait(ray_obj_refs, num_returns=len(ray_obj_refs))
-    print("Finished waiting for parallel downloads.")
-
-    # print("Zipping now...")
-    # zipped = zipfile.ZipFile('../RS-Skins.zip', 'w', zipfile.ZIP_DEFLATED)
-
+    for dl_list in [Folders["Folders_RS"], Folders["Folders_RSC"], Folders["Folders_BIN"]]:
+        for item in dl_list:
+            download_root_folder(item["dcs-codename"], item["gdrive-path"], service)
+    
     for root, dirs, files in os.walk(os.getcwd()):
         for name in files:
             if fnmatch.fnmatch(name.lower(), 'readme*.txt'): # Don't zip readmes
                 print("Removing %s" % os.path.join(root, name))
                 os.remove(os.path.join(root, name))
             else:
-                # zipped.write(os.path.join(root, name), arcname=os.path.relpath(os.path.join(root, name), os.getcwd())) # Add to zip
-                # os.remove(os.path.join(root, name)) # Remove after zipping
                 pass
 
 if __name__ == '__main__':
