d328101975fec25689b4852581df49689aad0eb3
Kaurin <2141359+Kaurin@users.noreply.github.com>
2022-12-05 19:55:33 +0000

    try downloader again

--- extract-file.ps1
+++ extract-file.ps1
@@ -3,8 +3,8 @@ param($7a_exec_path, $destination_dir, $archive_path)
 # Exit on any error
 $ErrorActionPreference = "Stop"
 
-# Write-Host "$7a_exec_path"
-# Write-Host "$destination_dir"
-# Write-Host "$archive_path"
+Write-Host "$7a_exec_path"
+Write-Host "$destination_dir"
+Write-Host "$archive_path"
 Set-Location -Path "$destination_dir"
 & "$7a_exec_path" -y -bd -bb0 x "$archive_path" | Out-Null
