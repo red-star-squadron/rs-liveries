90abd89bd4289d07d003f732ba9cb51dfa50dd76
Kaurin <2141359+Kaurin@users.noreply.github.com>
2023-11-03 20:36:57 +0000

    Updated self-hosted runner docker scripts

--- scripts/runner_start.sh
+++ scripts/runner_start.sh
@@ -1,16 +1,50 @@
 #!/usr/bin/env bash
 
 set -e
+set -o pipefail
 
-docker volume create --driver local \
+ORG=red-star-squadron
+REPO=rs-liveries
+
+gh_get_token() {
+  gh api \
+    --method POST \
+    -H 'Accept: application/vnd.github+json' \
+    -H 'X-GitHub-Api-Version: 2022-11-28' \
+    /repos/$ORG/$REPO/actions/runners/registration-token
+}
+
+mydocker() {
+  docker -l error "$@"
+}
+
+if mydocker inspect -f '{{.State.Running}}' "runner" > /dev/null 2>&1
+  then
+  echo "Clean up existing docker resources"
+  mydocker stop runner > /dev/null
+  mydocker rm runner > /dev/null
+  mydocker volume rm gh_tmpfs_vol > /dev/null
+fi
+
+echo "Create the docker volume"
+mydocker volume create --driver local \
     --opt type=tmpfs \
     --opt device=tmpfs \
-    gh_tmpfs_vol || true
+    gh_tmpfs_vol > /dev/null
 
-docker run \
+
+echo "Grab GH hosted runner token"
+REG_TOKEN="$(gh_get_token | jq -r .token)"
+
+echo "Start the gh hosted runner in docker"
+mydocker run \
   --detach \
-  --env ORGANIZATION=red-star-squadron/rs-liveries \
-  --env REG_TOKEN="REDACTED" \
+  --env ORGANIZATION="${ORG}" \
+  --env REG_TOKEN="${REG_TOKEN}" \
+  --env REPO="${REPO}" \
   --name runner \
   -v gh_tmpfs_vol:/home/docker/actions-runner/_work \
-  gh-runner-image
+  gh-runner-image > /dev/null
+
+echo "Check the status of this docker container with:"
+echo "docker logs -f runner"
