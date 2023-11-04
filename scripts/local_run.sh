f5df4e8deb2399963eb8c0709ff1c39c6a3aa2d9
Kaurin <2141359+Kaurin@users.noreply.github.com>
2023-11-04 08:58:01 +0000

    Unmount before mounting Staging and Compressed dirs

--- scripts/local_run.sh
+++ scripts/local_run.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
 set -e
+set -o pipefail
+
+# Warning, this will delete any files previously in Staging and Compressed dirs
+echo "Cleaning up Staging and Compressed dirs"
+sudo umount Staging || true
+sudo umount Compressed || true
+
+mkdir -p Staging
+mkdir -p Compressed
+
+# Now we can mount
+sudo mount -t tmpfs -o uid=1000 tmpfs Staging/
+sudo mount -t tmpfs -o uid=1000 tmpfs Compressed/
 
 pipenv run python rs_liveries_downloader.py
 
