66dc7e3c03d2a6f02240ec788bbed8b84964ba41
Milos Kaurin <2141359+Kaurin@users.noreply.github.com>
2024-05-28 07:57:50 +0100

    feat: Asset deletion script feature complete

--- scripts/delete_gh_release_assets.sh
+++ scripts/delete_gh_release_assets.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
+set -o pipefail
 
 # Delete all 7z files from a GitHub release
 # Usage: delete_gh_release_assets <release>
@@ -9,18 +10,8 @@ function delete_gh_release_assets() {
   gh release view "$1"  --json assets -q '.assets[].name' | grep '\.7z$' | xargs -n1 gh release delete-asset "$1"
 }
 
-# You can get a list of releases by running:
-# gh release list
+export -f delete_gh_release_assets
 
-delete_gh_release_assets "v0.9.13+2023-06-18-001"
-delete_gh_release_assets "v0.9.13+2023-06-01-001"
-delete_gh_release_assets "v0.9.13+2023-05-31-001"
-delete_gh_release_assets "v0.9.12+2023-05-07-001"
-delete_gh_release_assets "v0.9.11+2023-03-31-002"
-delete_gh_release_assets "v0.9.10+2023-03-17-001"
-delete_gh_release_assets "v0.9.9+2023-03-08-001"
-delete_gh_release_assets "v0.9.9+2023-03-07-001"
-delete_gh_release_assets "v0.9.8+2023-03-05-001"
-delete_gh_release_assets "v0.9.8+2023-02-26-001"
-delete_gh_release_assets "v0.9.7+2023-01-29-001"
-delete_gh_release_assets "v0.9.6+2022-12-15-001"
+# Deletes all assets from all releases except from the latest release
+# Documentation: Call bash function via xargs: https://unix.stackexchange.com/questions/158564/how-to-use-defined-function-with-xargs
+gh release list --json tagName -q '.[1:].[].tagName' | xargs -n1 bash -c 'delete_gh_release_assets "$@"' _
