d4b20d0c009fd8a855ee2f624a87a7d4cc6535cd
Milos Kaurin <2141359+Kaurin@users.noreply.github.com>
2024-08-31 12:43:40 +0100

    chore: Local runner updates * Ensure latest underlying image * Bump RUNNER_VERSION

--- scripts/runner_build.sh
+++ scripts/runner_build.sh
@@ -3,5 +3,5 @@
 set -e
 
 pushd "${0%/*}" || exit 1 # Dir of the script
-podman build --tag gh-runner-image gh-self-hosted-runner-podman
+podman build --pull --tag gh-runner-image gh-self-hosted-runner-podman
 popd || exit 1
